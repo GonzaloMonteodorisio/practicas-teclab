58e0c55be2ed3343cbe00ece937b9583dfcd0cfb
GonzaloMonteodorisio <gonzalomonteodorisio@gmail.com>
2024-02-22 16:46:37 -0300

    Added example Arrays Java

---
+++ ejercicios_lectura/modulo-3/Arreglos.java
@@ -0,0 +1,15 @@
+public class Arreglos {
+  public static void main(String[] args) {
+    int nums[] = new int[5];
+    
+    nums[0] = 100;
+    nums[1] = 200;
+    nums[2] = 300;
+    nums[3] = 400;
+    nums[4] = 500;
+
+    for (int i = 0; i < 5; i++) {
+      System.out.println(nums[i]);
+    }
+  }
+}
